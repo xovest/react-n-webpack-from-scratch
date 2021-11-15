d405d7022d24ffbddb1414881529111586a8e423
xovest <79008917+xovest@users.noreply.github.com>
2021-11-15 16:32:43 +0200

    the way the webpack thinks is pretty cool ngl

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'indexBundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
